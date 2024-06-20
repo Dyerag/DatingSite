12bed35aacc54c600befb6577e29364ad131e71a
Dyerag <dylaneric2003@gmail.com>
2024-06-20 10:51:28 +0200

    Added a check to prevent non-deleted accounts with the same username from existing simultaneously.

--- Components/Pages/AccountPage.razor.cs
+++ Components/Pages/AccountPage.razor.cs
@@ -8,6 +8,7 @@ namespace DatingSite.Components.Pages
     public partial class AccountPage
     {
         public Account CurrentAccount { get; set; } = new();
+        public bool UsernameTaken = false;
 
         protected override async Task OnParametersSetAsync()
         {
@@ -17,24 +18,49 @@ namespace DatingSite.Components.Pages
                 if (account != null)
                 {
                     CurrentAccount = account;
-                    
+
                 }
             }
         }
 
         async Task HandleSubmit()
         {
+            // If this is an existing account thats being edited
             if (userservice.Id is not null)
             {
+
                 await AccountService.UpdateAccount(CurrentAccount, (int)userservice.Id);
                 navigationManager.NavigateTo("/Details");
             }
+            // If this is a new account
             else
             {
-                await AccountService.CreateAccount(CurrentAccount);
-                userservice.SetCurrentUser(CurrentAccount);
+                //grabs every account in the database to compare usernames
+                var existingAccounts = await AccountService.GetAllAccounts();
+                //to keep track of how many accounts have been checked
+                int accountsChecked = 0;
 
-                navigationManager.NavigateTo("/Create Profile");
+                //goes throug every accounts username
+                foreach (var account in existingAccounts)
+                {
+                    accountsChecked++;
+                    UsernameTaken = false;
+                    // compares the usernames of the new account to the existing accounts
+                    // If the username is taken, notify the user that they can't use it
+                    if (account.Username == CurrentAccount.Username && account.IsDeleted == false)
+                    {
+                        UsernameTaken = true;
+                    }
+                    // If the username isn't taken, and the program has compared every account. Add the new account
+                    else if (accountsChecked >= existingAccounts.Count)
+                    {
+                        await AccountService.CreateAccount(CurrentAccount);
+                        userservice.SetCurrentUser(CurrentAccount);
+
+                        navigationManager.NavigateTo("/Create Profile");
+                    }
+
+                }
             }
         }
     }
